b6866b9e11bfa62ba4450ca9fd717044b51c6ec2
Sachin Kesarwani <112760394+Sachin-Kesarwani@users.noreply.github.com>
2023-03-28 14:46:09 +0530

    comp

--- eyecart-app/src/index.js
+++ eyecart-app/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+import {ChakraProvider} from "@chakra-ui/react"
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ChakraProvider>
+  <BrowserRouter>
+  
+  <App />
+
+</BrowserRouter>
+  </ChakraProvider>
+
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
