0866f0a6d0f8756eef8df55ce8f35d05164a30c5
Uzair <uzairmd073@gmail.com>
2023-03-29 11:47:53 +0530

    Cart-Page is done

---
+++ eyecart-app/src/Cart_components/Cart_Pro_Card.jsx
@@ -0,0 +1,55 @@
+import { Box, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import '../Cart_pages/cart.css'
+const Cart_pro_card = () => {
+  return (
+    <Box>
+    
+<Box className="cart_card">
+
+<Box>
+    <Image width={"150px"} height={"90px"} src="https://static5.lenskart.com/media/catalog/product/pro/1/thumbnail/628x301/9df78eab33525d08d6e5fb8d27136e95//v/i/black-full-rim-wayfarer-vincent-chase-glam-slam-vc-s15040-c1-sunglasses_g_8753_01_09_2022.jpg" alt="" />
+</Box>
+<Box>
+  <Text mb={"15px"}>
+     Black Full Rim Wayfarer Sunglasses
+    </Text>
+    <br />
+<hr style={{color:"grey",border: "2px dotted",borderStyle: "none none dotted"}} />
+  <Text mt={"10px"} mb={"10px"}>
+  Final Price   
+   </Text>
+<hr style={{color:"grey",border: "2px dotted",borderStyle: "none none dotted"}} />
+  <Text mt={"20px"}>
+ <a href="">
+     Remove 
+    </a>
+     <a href="">
+           |   Repeat
+        </a>
+    </Text>
+
+ 
+<hr />
+
+</Box>
+<Box display={"flex"} flexDirection={"column"}>
+<Text mb={"40px"}>₹1599
+
+</Text>
+<Text mb={"50px"}>₹1599
+
+</Text>
+
+
+</Box>
+
+
+</Box>
+
+
+    </Box>
+  )
+}
+
+export default Cart_pro_card
---
+++ eyecart-app/src/Cart_pages/Cart.jsx
@@ -0,0 +1,101 @@
+import { Box, Button, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import logo from '../assets/logo.jpg'
+import ButtonComp from '../Cart_components/ButtonComp'
+import Cart_pro_card from '../Cart_components/Cart_Pro_Card'
+import './cart.css'
+const Cart = () => {
+  return (
+    <Box style={{fontFamily: 'Poppins'}} bgColor={"#f8f8f8"} width={"100%"} height={"100vh"}>
+<Box border={"1px solid red"} height={"100px"} width={"100%"} color={"black"}  bgColor={"white"}>
+    <Box height={"100%"} width={"80%"} display={"flex"} justifyContent={"space-between"} margin={"auto"} border={"1px solid green"}>
+    <img  className={"logo"} src={"https://static.lenskart.com/media/desktop/img/site-images/main_logo.svg"} alt="" />
+        
+    <span>
+    <span>
+        <img width={"20px"} src="https://static1.lenskart.com/media/desktop/img/DesignStudioIcons/Shield.svg" alt="" />
+        100% safe and secure
+        </span>
+        
+    </span>
+    </Box>
+</Box>
+   <Box className="cart_container" style={{display:"flex",width:"70%",margin:"auto",marginTop:"50px"}}>
+  <Box className="first_box">
+  <Text color={"black"} textAlign={"center"} mb={"10px"}>
+Cart(1 items)
+
+    </Text>
+<Cart_pro_card />
+  </Box>
+<Box className="second_box">
+<h4>
+Bill Details
+</h4>
+
+
+<Box className="box1" padding={"10px"}>
+    <Box display={"flex"} justifyContent={"space-between"} width={"100%"}>
+        <Text>Total Price</Text>
+        <Text>₹1599</Text>
+    </Box>
+    <Box display={"flex"} justifyContent={"space-between"} width={"100%"}>
+        <Text>Tax</Text>
+        <Text>₹1599</Text>
+    </Box>
+    <Box display={"flex"} justifyContent={"space-between"} width={"100%"}>
+        <Text>Total Payable</Text>
+        <Text>₹1599</Text>
+    </Box>
+</Box>
+<Box className="box2" bgColor={"rgb(255, 243, 199)"} border={"1px solid #e0c67c"}>
+
+   <Text>
+     Add Gold Membership and get
+    </Text>
+   
+    <Text mt={"10px"} mb={"10px"}>
+2 Sunglases for ₹1599+(tax)
+
+    </Text>
+
+<hr  style={{color:"grey",border: "2px dotted",borderStyle: "none none dotted"}} />
+<Box display={"flex"} justifyContent={"space-between"}>
+<Text mt={"10px"}>
+     Add Gold & Choose second pair
+    </Text>
+     <Image cursor={"pointer"} src="https://static1.lenskart.com/media/desktop/img/DesignStudioIcons/OvalButton.svg" alt="" />
+
+</Box>
+</Box>
+<Box className="box3">
+   
+
+    <Box padding={"10px"} display={"flex"} justifyContent={"space-between"}>
+        <Box>
+
+    <Text cursor={"pointer"}>Apply Coupens</Text>
+        </Box>
+
+    <Text cursor={"pointer"}>Check available offers</Text>
+     <Image cursor={"pointer"} src="https://static1.lenskart.com/media/desktop/img/DesignStudioIcons/OvalButton.svg" alt="" />
+
+    </Box>
+</Box>
+{/* <ButtonComp /> */}
+<Button border={"1px solid green"} bgColor={"#11daac"} padding={"20px"} borderRadius={"20px"} className={"checkout_butt"}>
+    Proceed To Checkout
+</Button>
+</Box>
+
+   </Box>
+     
+
+
+
+
+    </Box>
+  )
+}
+
+export default Cart
