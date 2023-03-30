5253122db07c0bc095c52b7906b2952b72d7d0a1
Uzair <uzairmd073@gmail.com>
2023-03-30 13:11:19 +0530

    data pulling

--- eyecart-app/src/Admin/Home.jsx
+++ eyecart-app/src/Admin/Home.jsx
@@ -1,12 +1,8 @@
-
-// import React, { useEffect } from 'react'
-// import { useDispatch,useState, useSelector } from 'react-redux'
-// import { GetAlldata } from '../Redux/AdminRedux/Action'
-// import { store } from '../Redux/store'
 import { Button, Heading, Image } from '@chakra-ui/react'
 import { Loading } from '../frontend/Components/Loading'
+import { ReactText, useState } from 'react';
 
-const Home = () => {
+ const Home = () => {
     let [loading,setLoading]=useState(false)
     function closegif(){
         setLoading(false)
@@ -19,93 +15,9 @@ const Home = () => {
   
     </div>
   )
-
-// export default Home
-import React, { ReactNode, useContext, useState } from 'react';
-import {
-  IconButton,
-  Avatar,
-  Box,
-  CloseButton,
-  Flex,
-  HStack,
-  VStack,
-  Icon,
-  useColorModeValue,
-  Link,
-  Drawer,
-  DrawerContent,
-  Text,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
-  Heading,
-  useConst,
-  Image,
-} from '@chakra-ui/react';
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-  FiBell,
-  FiChevronDown,
-  FiUser,
-  FiBox
-} from 'react-icons/fi';
-// import { IconType } from 'react-icons';
-import { ReactText } from 'react';
-import { AuthContext } from './Context/Context';
-import logo from "./logo.jpg"
-
-const LinkItems= [
-  { id:1 ,name: 'Home', icon:FiUser },
-  { id:2,name: 'All Products', icon: FiBox },
-  { id:3 ,name: 'Explore', icon: FiCompass },
-  {id:4, name: 'Favourites', icon: FiStar },
-  { id:5,name: 'Settings', icon: FiSettings },
-];
-
-export default function AdminHome({children,}) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-
-
-
-  return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
-      />
-      <Drawer
-        autoFocus={false}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full">
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        
-        </DrawerContent>
-      </Drawer>
-      {/* mobilenav */}
-      <MobileNav onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        {children}
-      </Box>
-      <Heading>Hiiiiiiiiiiiiiiiiii</Heading>
-    </Box>
-  );
-
-}
+  }
 
 export default Home
+
+
+
--- eyecart-app/src/frontend/fw22_0314_Components/Pay_btn.jsx
+++ eyecart-app/src/frontend/fw22_0314_Components/Pay_btn.jsx
@@ -1,12 +1,86 @@
 import { Button } from '@chakra-ui/react'
+import axios from 'axios';
 import React from 'react'
 
+function loadScript(src) {
+  return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+          resolve(true);
+      };
+      script.onerror = () => {
+          resolve(false);
+      };
+      document.body.appendChild(script);
+  });
+}
+
+
+
+
+async function displayRazorpay() {
+  const res = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+  );
+
+  if (!res) {
+      alert("Razorpay SDK failed to load. Are you online?");
+      return;
+  }
+
+  // creating a new order
+  const result = await axios.post("http://localhost:7500/payment/orders");
+
+  if (!result) {
+      alert("Server error. Are you online?");
+      return;
+  }
+
+  // Getting the order details back
+  const { amount, id: order_id, currency } = result.data;
+let logo ="https://static.lenskart.com/media/desktop/img/site-images/main_logo.svg"
+  const options = {
+      key: "rzp_test_r6FiJfddJh76SI", // Enter the Key ID generated from the Dashboard
+      amount: amount.toString(),
+      currency: currency,
+      name: "Soumya Corp.",
+      description: "Test Transaction",
+      image: {logo},
+      order_id: order_id,
+      handler: async function (response) {
+          const data = {
+              orderCreationId: order_id,
+              razorpayPaymentId: response.razorpay_payment_id,
+              razorpayOrderId: response.razorpay_order_id,
+              razorpaySignature: response.razorpay_signature,
+          };
+
+          const result = await axios.post("http://localhost:5000/payment/success", data);
+
+          alert(result.data.msg);
+      },
+      prefill: {
+          name: "Soumya Dey",
+          email: "SoumyaDey@example.com",
+          contact: "9999999999",
+      },
+      notes: {
+          address: "Soumya Dey Corporate Office",
+      },
+      theme: {
+          color: "#61dafb",
+      },
+  };
+
+  const paymentObject = new window.Razorpay(options);
+  paymentObject.open();
+}
 const Pay_btn = () => {
   return (
-    <Button>
-    Submit
-   <form><script src="https://checkout.razorpay.com/v1/payment-button.js" data-payment_button_id="pl_LXLKqICz649aST" async> </script> </form>
-   </Button>
+    <Button bgColor={"black"} className="App-link" onClick={displayRazorpay}>
+    Pay ₹500
+</Button>
   )
 }
 
