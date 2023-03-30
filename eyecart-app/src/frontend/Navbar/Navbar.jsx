4ef79eda4cc8c65453c07f031b921dbe6b91011f
bakshay96 <akshaymali.edu@gmail.com>
2023-03-30 07:57:47 +0530

    navbar updated

--- eyecart-app/src/frontend/Navbar/Navbar.jsx
+++ eyecart-app/src/frontend/Navbar/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import { NavCard1 } from './NavCard'
+import { NavbarCard2, NavbarCard3, NavCard1 } from './NavCard'
+
+
 
 export const Navbar = () => {
   return (
     <div>
       <NavCard1 />
+     <NavbarCard2/>
+     <NavbarCard3/>
+     
     </div>
   )
 }
