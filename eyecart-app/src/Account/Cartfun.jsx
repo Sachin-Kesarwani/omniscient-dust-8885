7fe42322475f277cba59d4b2c93ea75965d1d9ff
Uzair <uzairmd073@gmail.com>
2023-04-01 18:19:35 +0530

    Accound section added

--- eyecart-app/src/Account/Cartfun.jsx
+++ eyecart-app/src/Account/Cartfun.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+// import { GetCart } from './Redux/CartRedux/actiontype';
+import { GetCartData } from './Redux/CartRedux/action';
+import { Loading } from '../frontend/Components/Loading';
 
 const Cartfun = () => {
-  return (
-    <div>Cartfun</div>
+    let dispatch = useDispatch();
+const [data,setdata]=useState([])
+    let {order,isLoading,isError} = useSelector((store) => store?.CartReducer);
+  const store=useSelector((store)=>store)
+    useEffect(() => {
+       async function myfun(){
+
+            let data=await dispatch(GetCartData)
+            setdata(order)
+        }
+      myfun()
+    }, []);
+
+    return isLoading?<Loading />:isError?<div>Error</div>:(
+        <div>
+
+        {data.map((el)=>{
+            return <div>
+                hello
+            </div>
+        })}
+
+
+    </div>
   )
 }
 
