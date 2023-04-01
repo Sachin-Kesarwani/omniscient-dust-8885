dba465f0ac5d0bcae59140e1a205ecee5884a62f
Uzair <uzairmd073@gmail.com>
2023-04-01 19:28:11 +0530

    some changes made

--- eyecart-app/src/Redux/CartRedux/actiontype.js
+++
@@ -1,8 +0,0 @@
-export let Loading = "cart/Loading";
-export let Error = "cart/Error";
-export let AddInCart = "cart/data";
-export let AddInWishList = "add/wishlist";
-export let DelfromWishlist = "del/wishlist";
-export let GetwishlistData = "get/wishlist";
-export let GetCart = "get/cartdata";
-export let GetorderData = "get/orderdata";
